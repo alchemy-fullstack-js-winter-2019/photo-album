95920cff75fbd33a08fbb6b0539f07a145aadc61
Dyanna Tolman <dyannast@gmail.com>
2019-02-18 13:27:22 -0800

    index.html and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
+const HtmlPlugin = require('html-webpack-plugin');
+//takes the template html and create for us
+const CleanPlugin = require('clean-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[hash].js'
-  //   //change this name on every build
-  // },
-  // module: {
-  
+    //change this name on every build
+  },
+  devServer: {
+    port: 7890
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' }),
+    new CleanPlugin('./dist')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
   }
 };
