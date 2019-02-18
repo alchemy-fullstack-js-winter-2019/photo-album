122a12810a651d99fb42f0ba19386e5c7a15f411
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-18 15:48:09 -0800

    more styling

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Dogs.css';
 
 function Dogs({ dogs }) {
   const listOfDogs = dogs.map(name => {
     return <li key={name}>{name}</li>;
   });
   return (
-    <main>
-      <ul>{listOfDogs}</ul>
-    </main>
+    <ul className={styles.dogs}>{listOfDogs}</ul>
   );
 }
 
