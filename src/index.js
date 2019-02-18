2f5b287bb10e83811992f8983140abd8dc58b4b7
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-18 14:07:53 -0800

    use App component with react render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-// console.log('hiiiiiiiii'); // eslint-disable-line no-console
+// // console.log('hiiiiiiiii'); // eslint-disable-line no-console
 
-const root = document.getElementById('root');
-const ul = document.createElement('ul');
-root.appendChild(ul);
+// const root = document.getElementById('root');
+// const ul = document.createElement('ul');
+// root.appendChild(ul);
 
-['one', 'two', 'three', 'four', 'five']
-  .map(name => {
-    const li = document.createElement('li');
-    li.textContent = name;
-    return li;
-  })
-  .forEach(li => ul.appendChild(li));
+// ['one', 'two', 'three', 'four', 'five']
+//   .map(name => {
+//     const li = document.createElement('li');
+//     li.textContent = name;
+//     return li;
+//   })
+//   .forEach(li => ul.appendChild(li));
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(
+  <App />, // this is 'jsx'
+  document.getElementById('root')
+);
