030ce74aaca828bf25b5a584f2dcf600f656877d
Paige Gorry <paigeegorry@gmail.com>
2019-02-18 14:06:48 -0800

    first react app appending to the dom!

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <span>
+      <h1>I am a title</h1>
+      <p>I am a p tag</p>
+    </span>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// append a ul to root
-// add 5 li to ul with dog names
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-const dogs = ['Pepper', 'Penelope', 'Pugsly', 'Peppa', 'Pig'];
-
-const root = document.getElementById('root');
-const ul = document.createElement('ul');
-root.appendChild(ul);
-
-dogs.map(d => {
-  const li = document.createElement('li');
-  li.textContent = d;
-  return li;
-})
-  .forEach(li => ul.appendChild(li));
+render(
+  <App />,
+  document.getElementById('root')
+)
