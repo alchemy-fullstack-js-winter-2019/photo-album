53a99345f13d8f561e3f74175d8abf837083a68b
Aaron Dennis <AMDennis1987@gmail.com>
2019-02-19 10:03:01 -0800

    fixed layout and added more bulldogs

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import PhotoAlbum from './PhotoAlbum';
+import styles from './App.css';
 import 'normalize.css';
 import bulldog1 from '../assets/photo1.jpg';
 import bulldog2 from '../assets/photo2.jpg';
+import bulldog3 from '../assets/photo3.jpg';
+import bulldog4 from '../assets/photo4.jpg';
+import bulldog5 from '../assets/photo5.jpg';
+import bulldog6 from '../assets/photo6.jpg';
 
 function App() {
-  const photoURL = [bulldog1, bulldog2];
+  const photoURL = [bulldog1, bulldog2, bulldog3, bulldog4, bulldog5, bulldog6];
 
   return (
     <>
-      <Header/>
-      <PhotoAlbum title='Bulldogs' url={photoURL}/>
-      <Footer />
+      <div className={styles.container}>
+        <Header/>
+        <PhotoAlbum title='Bulldogs' url={photoURL}/>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styles from './photoAlbum.css';
+import styles from './photoAlbum.css';
 import Photos from './Photos';
 
 
 function PhotoAlbum({ title,  url }) {
   return (
-    <>
+    <main className={styles.photoAlbum}>
       <h1>{ title }</h1>
       <Photos url={ url } />
-    </>
+    </main>
   );
 
 }
