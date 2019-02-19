6b63dba6a626747b8b4969dc3eeb0b5beeec6420
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-19 08:34:42 -0800

    can see albums

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,16 +1,20 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Photos from './Photos';
 
 function PhotoAlbum({ title, photos }) {
+  
+
   return (
     <div>
-      <h1>HI</h1>
+      <h1>{title}</h1>
+      <Photos photos={photos}/>
     </div>
   );
 }
-// PhotoAlbum.PropTypes = {
-//   title: PropTypes.string.isRequired,
-//   photos: PropTypes
-// };
+PhotoAlbum.propTypes = {
+  title: PropTypes.string.isRequired,
+  photos: PropTypes.array.isRequired
+};
 
 export default PhotoAlbum;
