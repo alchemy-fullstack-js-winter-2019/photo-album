849990c13d181e7fc33fbb8bc58b4c2c83cf85a2
Shaba9 <shaba92@gmail.com>
2019-02-19 09:07:06 -0800

    PhotoAlbum css added

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Photos from './Photos';
+import styles from './PhotoAlbum.css';
 
 function PhotoAlbum({ title, urls }) {
   return (
     <>
-      <h2>Album: {title}</h2>
+      <h2 className={styles.title}>Album: {title}</h2>
       <Photos urls={urls} />
     </>
   );
