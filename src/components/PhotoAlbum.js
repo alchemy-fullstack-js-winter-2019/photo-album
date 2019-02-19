240d737a850e66b4dbf27f0da2d6af8e88a87b0d
Paige Gorry <paigeegorry@gmail.com>
2019-02-18 16:39:23 -0800

    photos component connected with PhotoAlbum component. photoAlbum has list of urls for images

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photos from './Photos';
 import styles from '../css/PhotoAlbum.css';
+import jupiter from '../assets/jupiter.jpg';
+import mercury from '../assets/mercury.jpg';
+import neptune from '../assets/neptune.jpg';
+import uranus from '../assets/uranus.jpg';
+import venus from '../assets/venus.jpg';
 
 function PhotoAlbum() {
+  const planets = [jupiter, mercury, neptune, uranus, venus];
+  console.log(planets);
   return (
     <main className={styles.main}>
       <h2>planetz</h2>
+      <Photos urls={planets}/>
     </main>
   );
 }
---
+++ src/components/Photos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Photos() {
+  return (
+    <ul>
+      <li>I am the photos component</li>
+    </ul>
+  );
+}
+
+export default Photos;
