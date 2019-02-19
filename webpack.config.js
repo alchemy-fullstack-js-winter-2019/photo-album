5d042e8d089dceb54923a826f2393d8b04b61a72
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-18 16:16:40 -0800

    final class updates

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' }),
     new CleanPlugin('./dist')
+    
   ],
   module: {
     rules: [
@@ -26,6 +27,41 @@ module.exports = {
             cacheDirectory: true
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')(),
+                require('postcss-nested')()
+              ]
+            }
+          }
+
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 }
+        },
       }
     ]
   }
