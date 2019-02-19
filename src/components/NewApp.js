021de49a6118c11f49e0d49d6de0995519691f43
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-18 20:11:31 -0800

    added more synth pics and linked in App instead of PhotoAlbum

--- src/components/NewApp.js
+++ src/components/NewApp.js
@@ -9,15 +9,25 @@ import PhotoAlbum from './PhotoAlbum';
 function NewApp() {
   const title = 'Cari\'s amazing Photo Album app';
   const name = 'Cari Palazzolo';
-  // const photo1 = '/src/assets/ms20.jpg';
-  // const photo2 = '/src/assets/kitten.jpg';
-  // const photo3 = '/src/assets/roland_juno6.jpg';
-  // const photos = [photo1, photo2, photo3];
+  const albumTitle = 'Synthesizers';
+  const photo1 = '/src/assets/ms20.jpg';
+  const photo2 = '/src/assets/kitten.jpg';
+  const photo3 = '/src/assets/roland_juno6.jpg';
+  const photo4 = '/src/assets/korg_ms2000.jpg';
+  const photo5 = '/src/assets/casio_ct401.jpg';
+  const photo6 = '/src/assets/crumar_stratus_2.jpg';
+  const photo7 = '/src/assets/roland_jupiter6.jpg';
+  const photo8 = '/src/assets/moog_opus3.jpg';
+  const photo9 = '/src/assets/yamaha_cs5.jpg';
+  const photo10 = '/src/assets/moog_minimoogd.jpg';
+  const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10];
   return (
     <>
       <Header title={title}/>
       {/* <Photos urls={photos} /> */}
-      <PhotoAlbum />
+      <PhotoAlbum title={albumTitle}
+        urls={photos}
+      />
       {/* <Photo url={photo1}/> */}
       <Footer name={name} />
     </>
--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Photos from './Photos';
+// import Photo from './Photo';
+import PropTypes from 'prop-types';
 
 // PhotoAlbum component should take a 
 // title and an array of urls. It should 
@@ -7,17 +9,21 @@ import Photos from './Photos';
 // the array of urls using the Photos component
 
 function PhotoAlbum({ title, urls }) {
-  const photo1 = '/src/assets/ms20.jpg';
-  const photo2 = '/src/assets/kitten.jpg';
-  const photo3 = '/src/assets/roland_juno6.jpg';
-  const photos = [photo1, photo2, photo3];
+  // const photo1 = '/src/assets/ms20.jpg';
+  // const photo2 = '/src/assets/kitten.jpg';
+  // const photo3 = '/src/assets/roland_juno6.jpg';
+  // const photos = [photo1, photo2, photo3];
   return (
     <>
-    <h1 title={title}>Synthesizers</h1>,
-    <Photos urls={photos}/>
+      <h1>{title}</h1>
+      <Photos urls={urls}/>
     </>
   );
 }
 
+PhotoAlbum.proptypes = {
+  title: PropTypes.string.isRequired,
+  urls: PropTypes.array.isRequired
+};
 
 export default PhotoAlbum;
