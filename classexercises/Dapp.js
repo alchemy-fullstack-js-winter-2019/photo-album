3984035a47b852f4da8d23896a984f7836f067bf
Carmen Perezchica <crperezchica@gmail.com>
2019-02-19 10:27:51 -0800

    notes

--- classexercises/Dapp.js
+++ classexercises/Dapp.js
@@ -2,15 +2,16 @@ import React, { Fragment } from 'react';
 import Header from '../src/components/Header';
 import Dogs from './Dogs';
 import Footer from '../src/components/Footer';
+import styles from 
 
 
 
 function App() {
-  const dogNames = ['snowball', 'cookie'];
+  const dogNames = [name: 'snowball', about:'good dog', name: 'cookie', about: 'ok dog'];
   return (
     <Fragment>
       <Header />
-      <Dogs dogs={dogNames} />
+      <Dogs namesOfdogs={dogNames} />
       <Footer />
     </Fragment>
    
--- classexercises/Dogs.js
+++ classexercises/Dogs.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import puppy1 from '../assets/puppy1.jpeg';
+import Dog from './Dog';
 
 
-function Dogs({ dogs }) {
-  const listOfDogs = dogs.map(name => {
+function Dogs({ namesOfDogs }) { //Dogs is the parent 
+  const listOfDogs = dogs.map(dog => { // or ({name, about })
     //const li = document.createElement('li');
     //li.textContent = name;
-    return <li key={name}>{name}</li>;
+    //const props = {
+    //   name: name,
+    //   about: about
+    // }
+    //Dog(props); Dog is a function that props
+    return <li key={name}>Dog name={dog.name} Dog about={dog.about}</li>; //passing the name to this dog component
+  //the li key={name} is a way to identify a list item. It's reacts way to identifies by key name
   });
   return (
     <ul>
       <img src={puppy1} />
+      <img src={puppy2} />
       {listOfDogs}
     </ul>
   );
