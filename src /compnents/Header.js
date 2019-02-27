2a8146568b100d0c5aa4d4fe6c430f2c174ed8a3
pickuppatterns <pickup.patterns@gmail.com>
2019-02-27 07:49:58 -0800

    updates Header

---
+++ src /compnents/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../css/Header.css';
+
+
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Photo Album</h1>
+    </header>
+  );
+}
+
+
+export default Header;
