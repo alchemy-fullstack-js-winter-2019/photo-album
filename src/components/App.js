4509481680512327c8ce535796920e9e90d39fcf
pickuppatterns <pickup.patterns@gmail.com>
2019-02-28 07:06:56 -0800

    first photo rendering to browser

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import PhotoAlbum from './PhotoAlbum';
+import dolls from '../assets/720A9917.jpg';
 
 
 
 
 function App() {
+  const photos = [dolls];
   return (
     <Fragment>
       <Header />
