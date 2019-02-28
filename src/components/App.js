150f5af8317ff7e4dee8a705b6783124166ffaaf
pickuppatterns <pickup.patterns@gmail.com>
2019-02-28 06:54:19 -0800

    migrates project to new directory

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>Michael Elliott Photography </p>
+    </footer>
+  );
+}
+
+
+export default Footer;
